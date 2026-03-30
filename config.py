0ced1c96683e8da012a02a753494de9878b6d48a
Jyprakash18 <jayPrakash092006@gmail.com>
2026-03-30 19:07:35 +0530

    Update BOT_TOKEN and DEFAULT_TAG values

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8655630907:AAF34iEzf3h8PO6RhgtHuDBmrFXZhtRARVk")
 # Default tag if user hasn't set one
-DEFAULT_TAG = "yourdefault-21"
+DEFAULT_TAG = "sumandealsx-21"
